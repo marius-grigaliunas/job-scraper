3f9e7f3894d0abdf592bea5b2426ac565500afb7
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2024-07-01 01:57:29 +0200

    Basic JS LinkedIn Job overview scraper

--- scraper.js
+++ scraper.js
@@ -7,9 +7,9 @@ const puppeteer = require('puppeteer');
 //puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
 
 const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
-link = "https://www.linkedin.com/jobs/search/?keywords=Software%20Developer&location=Belgium&geoId=100565514&f_TPR=r604800&position=1&pageNum=0";
+const link = "https://www.linkedin.com/jobs/search/?keywords=Software%20Developer&location=Belgium&geoId=100565514&f_TPR=r604800&position=1&pageNum=0";
 const fs = require("fs/promises");
-const { title } = require('process');
+
 
 const scraper = async () => {
     const browser = await puppeteer.launch({
@@ -20,43 +20,37 @@ const scraper = async () => {
     page.setUserAgent(ua);
 
 
-    //await scrollToBottom(page);
-
     const jobTitles = await page.$$eval("#main-content > section.two-pane-serp-page__results-list > ul > li > div > div.base-search-card__info > h3", titles => {
         return titles.map(title => title.textContent.replace(/^\s+|\s+$/g, ''));
     });
 
     await fs.writeFile("titles.txt", jobTitles.join("\r\n"));
 
-    const jobs = await page.$$eval("#main-content > section.two-pane-serp-page__results-list > ul > li")
-
-    for(const job of jobs) {
-      let title = job.
-    } 
-
-    await fs.writeFile("jobs.txt", jobTitles.join("\r\n"));
-
-
-    /*
     const jobs = await page.$$eval("#main-content > section.two-pane-serp-page__results-list > ul > li", offers => {
-      return offers.map(offer => {
-        title = page.evaluate("div.base-search-card__info > h3", title => title.textContent);
-        //title = titleElement.textContent.replace(/^\s+|\s+$/g, '');
+      return offers.map(listElement => {
+        const titleElement = listElement.querySelector("div > div.base-search-card__info > h3");
+        const title = titleElement.textContent.trim();
+
+        const companyElement = listElement.querySelector("div > div.base-search-card__info > h4");
+        const company = companyElement.textContent.trim();
+
+        const locationElement = listElement.querySelector("div > div.base-search-card__info > div > span");
+        const location = locationElement.textContent.trim();
+        
+        const timeElement = listElement.querySelector("div > div.base-search-card__info > div > time");
+        const time = timeElement.dateTime.trim();
+
+        const linkElement = listElement.querySelector("div > a");
+        const url = linkElement.href.toString();
+
+        return title + ';' + company + ';' + location + ';' + time + ';' + url + ';';
       })
     });
 
-    await fs.writeFile("jobs.txt", jobs.join("\r\n"));
 
-    */
+    await fs.writeFile("jobs.txt", jobs.join("\r\n"));
 
     browser.close();
 };
-
-/*async function scrollToBottom(page) {
-    const distance = 100; // should be less than or equal to window.innerHeight
-    while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
-      await page.evaluate((y) => { document.scrollingElement.scrollBy(0, y); }, distance);
-    }
-  }*/
 
 scraper();
